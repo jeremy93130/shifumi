23a7b62b85cc571aea724a6be2f6913c267fde8d
jeremy Dubrulle <https://github.com/jeremy93130/jour3>
2021-10-04 17:23:49 +0200

    Avancement

---
+++ index.js
@@ -0,0 +1,8 @@
+function show() {
+    var image = document.getElementById("rock")
+    image.setAttribute("src", "https://www.jerome-reaux-creations.fr/DVP/codepen/shifumi/1-pierre.jpg")
+}
+
+function showing () {
+    var button = document.getElementById
+}
