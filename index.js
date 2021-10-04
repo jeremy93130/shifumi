32f86e35bdcc40326a3153cb70088f8f729d602a
jeremy Dubrulle <https://github.com/jeremy93130/jour3>
2021-10-04 20:23:40 +0200

    Avancement Shifumi

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-function show() {
-    var image = document.getElementById("rock")
+function show(rock) {
+    var button = document.getElementById("rock")
+    var image = document.getElementById("rps")
     image.setAttribute("src", "https://www.jerome-reaux-creations.fr/DVP/codepen/shifumi/1-pierre.jpg")
+    console.log("Pierre !")
 }
 
-function showing () {
-    var button = document.getElementById
+function showing(paper) {
+    var button = document.getElementById("paper")
+    var image = document.getElementById("rps")
+    image.setAttribute("src", "https://www.jerome-reaux-creations.fr/DVP/codepen/shifumi/2-feuille.jpg")
+    console.log("Papier !")
+}
+
+function showed(scissors){
+    var button = document.getElementById("scissors")
+    var image = document.getElementById("rps")
+    image.setAttribute("src", "https://jerome-reaux-creations.fr/DVP/codepen/shifumi/3-ciseaux.jpg")
+    console.log("Ciseaux !")
 }
